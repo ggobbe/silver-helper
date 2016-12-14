86354828986e9fc9adf874f01755c4a83a436c7a
ggobbe <guillaume.gobbe@gmail.com>
2016-12-15 00:05:19 +0100

    Update code to make it more readable

--- silver-helper.js
+++ silver-helper.js
@@ -7,37 +7,36 @@ if (url.endsWith("auberge.php")) {
 
 } else if (url.endsWith("rating")) {
     // TODO work in progress, need to get Pseudo and to put link after iframe src change
-    var iframe = $('iframe[src^="/membres.php"]');
-    var iframeDOM = iframe.contents();
-    var rating = iframeDOM.find("td.default")[1];
-    var regex = /Vous êtes classé (\d+)ème sur \d+ membres/g;
-    var result = regex.exec(rating.innerText);
-    if (result != null) {
-        var position = +result[1];
-        var total = +result[2];
+    var iframeElement = $('iframe[src^="/membres.php"]');
+    var iframe = iframeElement.contents();
+    var rankElement = iframe.find("td.default")[1];
+    var rankRegex = /Vous êtes classé (\d+)ème sur \d+ membres/g;
+    var rankResult = rankRegex.exec(rankElement.innerText);
+    if (rankResult != null) {
+        var rank = +rankResult[1];
 
-        var skip = position - 25;
-        if (skip < 0) skip = 0;
+        var pageSkip = rank - 25;
+        if (pageSkip < 0) pageSkip = 0;
 
-        var nextLink = iframeDOM.find('a[href^="?begin="]').first();
-        var clRegex = /cl=(\d+)/g;
-        var clResult = clRegex.exec(nextLink.attr("href"));
-        var cl = 0;
-        if (clResult != null) {
-            cl = clResult[1];
-            rating.append(document.createElement("br"));
+        var nextPageLink = iframe.find('a[href^="?begin="]').first();
+        var categoryRegex = /cl=(\d+)/g;
+        var categoryResult = categoryRegex.exec(nextPageLink.attr("href"));
+        var category = 0;
+        if (categoryResult != null) {
+            category = categoryResult[1];
+            rankElement.append(document.createElement("br"));
             var posLink = document.createElement("a");
-            $(posLink).attr("href", "membres.php?begin=" + skip + "&cl=" + cl);
+            $(posLink).attr("href", "membres.php?begin=" + pageSkip + "&cl=" + category);
             $(posLink).attr("class", "whiteLink");
             $(posLink).css("fontWeight", "bold");
             $(posLink).append(document.createTextNode("Naviguer vers ma page"));
-            rating.append(posLink);
+            rankElement.append(posLink);
 
-            iframeDOM.find('td[align="left"] > span.default').each(function (index, value) {
-                if (value.innerText.indexOf("Pseudo") != -1) {
-                    $(this).closest("tr").css("backgroundColor", "#000");
-                }
-            });
+            // iframe.find('td[align="left"] > span.default').each(function (index, value) {
+            //     if (value.innerText.indexOf("Pseudo") != -1) {
+            //         $(this).closest("tr").css("backgroundColor", "#000");
+            //     }
+            // });
         }
     }
 }
