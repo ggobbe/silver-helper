b7410caaa5ce6b3ae8bc89b3d43f41debee3f7c5
ggobbe <guillaume.gobbe@gmail.com>
2016-12-17 12:29:47 +0100

    Make crawler generic to extract items from pages

--- neosw-crawler/Program.cs
+++ neosw-crawler/Program.cs
@@ -13,41 +13,46 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var potions = GetPotionsId();
-            string json = JsonConvert.SerializeObject(potions);
+            var lifePotions = ExtractItemsIdFromUrl("https://sw.neosw.be/?psw=list_potion&type=pv");
+            var manaPotions = ExtractItemsIdFromUrl("https://sw.neosw.be/?psw=list_potion&type=mana");
+            var constitutionBoosts = ExtractItemsIdFromUrl("https://sw.neosw.be/?psw=list_potion&type=constitution");
+            var forceBoosts = ExtractItemsIdFromUrl("https://sw.neosw.be/?psw=list_potion&type=force");
+            var agiliteBoosts = ExtractItemsIdFromUrl("https://sw.neosw.be/?psw=list_potion&type=agilite");
+            var intelligenceBoosts = ExtractItemsIdFromUrl("https://sw.neosw.be/?psw=list_potion&type=intelligence");
+
+            var items = new
+            {
+                Potions = lifePotions.Concat(manaPotions),
+                Boosts = constitutionBoosts.Concat(forceBoosts).Concat(agiliteBoosts).Concat(intelligenceBoosts)
+            };
+
+            string json = JsonConvert.SerializeObject(items);
             Console.WriteLine(json);
         }
 
-        private static IEnumerable<int> GetPotionsId()
+        private static IEnumerable<int> ExtractItemsIdFromUrl(string url)
         {
             var config = Configuration.Default.WithDefaultLoader();
-            var address = "https://sw.neosw.be/?psw=list_potion&type=pv";
-            var document = BrowsingContext.New(config).OpenAsync(address).Result;
-            var cellSelector = "img[src^=\"img/sw/obj/obj\"]";
+            var document = BrowsingContext.New(config).OpenAsync(url).Result;
+            var imgSelector = "img[src^=\"img/sw/obj/obj\"]";
 
-            var items = document.QuerySelectorAll(cellSelector);
-            var images = items.Select(i => i.GetAttribute("src"));
+            var images = document.QuerySelectorAll(imgSelector);
+            var imagesSrc = images.Select(i => i.GetAttribute("src"));
 
-            var pattern = new Regex(@"img\/sw\/obj\/obj(?<id>\d+).(?:png|gif|jpg)");
-            
-            foreach(var image in images){
-                var match = pattern.Match(image);
+            var idPattern = new Regex(@"img\/sw\/obj\/obj(?<id>\d+).(?:png|gif|jpg)");
+
+            foreach (var src in imagesSrc)
+            {
+                var match = idPattern.Match(src);
                 var id = match.Groups["id"];
-                
+
                 var result = 0;
-                if(!int.TryParse(id.Value, out result)){
-                    throw new InvalidCastException($"Id could not be parsed in URL: " + image);
+                if (!int.TryParse(id.Value, out result))
+                {
+                    throw new InvalidCastException($"Id could not be parsed in URL: " + src);
                 }
                 yield return int.Parse(id.Value);
             }
         }
-
-        // string text = "ImageDimension=655x0;ThumbnailDimension=0x0";
-        // Regex pattern = new Regex(@"ImageDimension=(?<imageWidth>\d+)x(?<imageHeight>\d+);ThumbnailDimension=(?<thumbWidth>\d+)x(?<thumbHeight>\d+)");
-        // Match match = pattern.Match(text);
-        // int imageWidth = int.Parse(match.Groups["imageWidth"].Value);
-        // int imageHeight = int.Parse(match.Groups["imageHeight"].Value);
-        // int thumbWidth = int.Parse(match.Groups["thumbWidth"].Value);
-        // int thumbHeight = int.Parse(match.Groups["thumbHeight"].Value);
     }
 }
